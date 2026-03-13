06672caa5c471af06e894f58a62a56f24ff2923c
Instituto SerClin <institutoserclin@gmail.com>
2026-03-13 14:36:09 -0500

    Primeiro envio do sistema

---
+++ config.js
@@ -0,0 +1,16 @@
+// Configuração extraída do seu console Firebase
+const firebaseConfig = {
+    apiKey: "AIzaSyCYFKI7w7Lhe4ljvrO13s7e8cERMsBVr2Q",
+    authDomain: "chaves-estacio.firebaseapp.com",
+    databaseURL: "https://chaves-estacio-default-rtdb.firebaseio.com",
+    projectId: "chaves-estacio",
+    storageBucket: "chaves-estacio.firebasestorage.app",
+    messagingSenderId: "1052463955885",
+    appId: "1:1052463955885:web:32abff964d95be9f95ab24",
+    measurementId: "G-CESXMKVRE0"
+};
+
+// Inicialização compatível com Scripts diretos (compat)
+firebase.initializeApp(firebaseConfig);
+const auth = firebase.auth();
+const db = firebase.database();
