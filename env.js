a4d9f2b8184f1ec1885f2ce907e3500191a597ef
rchaves77 <140459080+rchaves77@users.noreply.github.com>
2026-03-31 05:57:19 +0000

    Deploying to gh-pages from @ rchaves77/controle-chaves@e9d9f28305e2977f6a572f287c4204d763251595 🚀

---
+++ env.js
@@ -0,0 +1,10 @@
+const firebaseConfig = {
+    apiKey: AIzaSyCTXsWaCrdgo4hNNDhTgRv-YYS0-S37FOc-AIzaSyCHWA0qu9eMS8Pu-u9Ioo3SKVB9Fn8aDfk,
+    authDomain: chaves-estacio.firebaseapp.com,
+    databaseURL: https://chaves-estacio-default-rtdb.firebaseio.com,
+    projectId: chaves-estacio,
+    storageBucket: chaves-estacio.firebasestorage.app,
+    messagingSenderId: 1052463955885,
+    appId: 1:1052463955885:web:32abff964d95be9f95ab24,
+    measurementId: G-CESXMKVRE0
+};
